266983ca1d0f3fb0d5c233fd13627e332c890e89
goodnessdevs <devswithgoody82@gmail.com>
2025-09-16 00:23:25 +0100

    new

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,9 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./components/ui/theme-provider.tsx";
 import { AuthContextProvider } from "./context/AuthContextProvider.tsx";
-import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HelmetProvider>
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
         <BrowserRouter>
           <AuthContextProvider>
@@ -17,6 +15,5 @@ createRoot(document.getElementById("root")!).render(
           </AuthContextProvider>
         </BrowserRouter>
       </ThemeProvider>
-    </HelmetProvider>
   </StrictMode>
 );
